10c582f8003f3cc950683da9d2cbaa1baaed5aa1
JoshuaPelealu <hasianjoshua@gmail.com>
2021-06-22 15:20:32 -0400

    Added some todos and useful code for later

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,38 @@
 
+
+//class Testo {
+//    interface run {
+//        operator fun invoke()
+//    }
+//
+//    class plus: run {
+//        val x = 1
+//
+//        override fun invoke() {
+//            println("RUNNING BOIS")
+//        }
+//    }
+//
+//    operator fun get(s: String): run {
+//        return plus()
+//    }
+//}
+//
+//fun DynamicallyInvokingAFunction() {
+//    val x = Testo()
+//    val w = "run"
+//
+//    x[w]()
+//}
+
 fun main() {
     shell()
+
 }
 
+/*
+TODO Make builtin operators
+TODO Make operators into it's own class
+TODO Make it so that new operators can be made
+
+ */
